8f085c46b34886ecfde10d7bd589a85704c04af1
feliposz <feliposz@gmail.com>
2024-04-16 05:14:09 -0300

    Respond to multiple PINGs

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,29 @@
 import * as net from "node:net";
 
-const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
-  connection.write("+PONG\r\n");
-  connection.end();
-  server.close();
+const ClientTimeout = 1000;
+const ServerTimeout = 1000;
+
+const server: net.Server = net.createServer();
+
+server.on("connection", (connection: net.Socket) => {
+  console.log("client connected");
+  connection.on("close", () => {
+    console.log("client disconnected");
+    connection.end();
+  });
+  connection.on("data", (data) => {
+    console.log("received data:", data);
+    connection.write("+PONG\r\n");
+  });
+  setTimeout(() => connection.end(), ClientTimeout);
 });
 
-server.listen(6379, "127.0.0.1");
+server.on("error", (err) => {
+  throw err;
+})
 
+server.listen(6379, "127.0.0.1", () => {
+  console.log("listening for connections");
+});
+
+setTimeout(() => server.close(), ServerTimeout);
