c6763d51b202428152bf0c28825948aeed8ebb2b
feliposz <feliposz@gmail.com>
2024-04-16 04:24:16 -0300

    Respond to PING

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,9 @@ import * as net from "node:net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  connection.write("+PONG\r\n");
+  connection.end();
+  server.close();
 });
 
 server.listen(6379, "127.0.0.1");
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+COURSE_DEFINITION=../redis-tester/internal/test_helpers/course_definition.yml
+export CODECRAFTERS_SUBMISSION_DIR=$(pwd)
+export CODECRAFTERS_TEST_CASES_JSON=$(yq -I0 '[.stages[] | {"slug": .slug, "tester_log_prefix": .slug, "title": (path | .[-1] + 1) + ") " + .name }]' -o=json $COURSE_DEFINITION)
+../redis-tester/redis-tester
