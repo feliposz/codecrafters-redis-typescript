b2b9310e6a0b53ed83b4cf519e9f5f749bb59267
feliposz <feliposz@gmail.com>
2024-04-30 17:42:52 -0300

    Updating local test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-COURSE_DEFINITION=../redis-tester/internal/test_helpers/course_definition.yml
+COURSE_DEFINITION=../../_others/redis-tester/internal/test_helpers/course_definition.yml
 export CODECRAFTERS_SUBMISSION_DIR=$(pwd)
 export CODECRAFTERS_TEST_CASES_JSON=$(yq -I0 '[.stages[] | {"slug": .slug, "tester_log_prefix": .slug, "title": (path | .[-1] + 1) + ") " + .name }]' -o=json $COURSE_DEFINITION)
-../redis-tester/redis-tester
+../../_others/redis-tester/redis-tester.exe
